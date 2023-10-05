d16176a5cf9a73ef990b0c212f1a698925036fa8
aknur <95418973+aknurkappar@users.noreply.github.com>
2023-10-05 20:54:17 +0600

    Initial Commit

---
+++ sortingNamesBeginnerHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sortingNamesBeginnerHW
+//
+//  Created by коня on 05.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
